f7a2e2e83faeee8997a085b7056f8b54fd130d98
YaliWang0523 <yali.wang.0523@gamil.com>
2014-12-15 22:27:32 +0800

    test

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var c : Float
+c=32.1
+var f : Float
+f=c*9/5+32
---
+++ ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Temptrans
+//
+//  Created by yali-mac on 2014/11/5.
+//  Copyright (c) 2014年 yali-mac. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var lblResult : UILabel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var c : Float
+        c = 28.7
+        var f : Float
+        f = tempTrans(c)
+        lblResult?.text="攝氏\(c) = 華氏\(f)"
+        
+}
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tempTrans(c : Float) ->Float
+    {
+        var f : Float
+        f = c*9/5+32
+        return f
+    }
+
+    
+
+}
+
